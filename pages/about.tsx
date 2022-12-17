5c05d4f233ec70ed7b95f81c5fc28837316e6555
John Tran <john.tran.engineer@gmail.com>
2022-12-17 13:36:46 +0700

    feat: components and layouts

---
+++ pages/about.tsx
@@ -0,0 +1,23 @@
+import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
+import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
+import styles from '../styles/Home.module.css';
+import { NextPageWithLayout } from './page';
+
+const About: NextPageWithLayout = () => {
+  return (
+    <section className={styles.main}>
+      <h1>About</h1>
+    </section>
+  );
+};
+
+export default About;
+
+About.getLayout = (page) => {
+  return (
+    <PrimaryLayout>
+      <SidebarLayout />
+      {page}
+    </PrimaryLayout>
+  );
+};
